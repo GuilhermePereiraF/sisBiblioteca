db59bde31a1ba0a76cda195a2c4646c17ff06564
guilherme.pereira10 <guilherme.pereira10@aluno.ifce.edu.br>
2023-10-27 09:24:41 -0300

    Salvar leitor

---
+++ controle/leitorControle.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'modelo/dominio/Leitor.php';
+require_once 'modelo/dao/LeitorDao.php';
+
+$leitorDao = new LeitorDao();
+
+$acao = isset($_REQUEST['acao']) ? $_REQUEST['acao'] : NULL;
+
+if ($acao == NULL) {
+    include 'pages/formLeitor.php';
+} else if ($acao == "salvar") {
+    $leitor = new Leitor();
+    $leitor->setNome($_POST['nome']);
+    $leitor->setNascimento($_POST['nascimento']);
+    $leitor->setCPF($_POST['cpf']);
+    $leitor->setRG($_POST['rg']);
+
+    $leitorDao->salvar($leitor);
+} else if ($acao == "listar") {
+    echo "listando...";
+} else if ($acao == "alterar") {
+    echo "alterando...";
+} else if ($acao == "excluir") {
+    echo "excluindo...";
+}
